741274f1374a646fa83326e5b34001d87b5fa0ed
Fahem Katache <Katache.fahem@gmail.com>
2025-09-09 16:09:08 +0200

    Initial Commit

---
+++ Agent ProTests/Agent_ProTests.swift
@@ -0,0 +1,17 @@
+//
+//  Agent_ProTests.swift
+//  Agent ProTests
+//
+//  Created by Jean-Berké Akdogan on 09/09/2025.
+//
+
+import Testing
+@testable import Agent_Pro
+
+struct Agent_ProTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
